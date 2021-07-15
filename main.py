9afcfa9d97aad8a64bca3ce63f1b1ae87764a645
monachary <monachary.kanna521@gmail.com>
2021-07-15 17:37:54 +0530

    uncommented another_print()

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def print_hi(name):
 def another_print():
     print("testing git")
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
